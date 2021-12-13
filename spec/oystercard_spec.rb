5b53b675007543efd4948b3f48ac582ab8ddcff5
Jason Humbert <jasonhumbert@outlook.com>
2021-12-13 21:03:43 +0000

    Add tests and impliment an Oystercard having an initial balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,15 @@
-
+require 'oystercard'
 
 describe Oystercard do
+  it { is_expected.to be_a Oystercard }
+  it { is_expected.to respond_to(:balance) }
+
+  subject(:card) { Oystercard.new }
+
+  describe '#balance' do
+    it 'has an initial balance of zero' do
+      expect(card.balance).to eq(0)
+    end
+  end
+
 end
